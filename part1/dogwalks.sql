11df2d7b7156e81abdcbf90e899c0abe41b3253d
FormerDays <tangpuran@outlook.com>
2025-06-20 15:35:37 +0000

    [automatic commit] 2025-06-20 15:35:37 UTC

--- part1/dogwalks.sql
+++ part1/dogwalks.sql
@@ -76,4 +76,4 @@ VALUES
   ((SELECT id FROM Dogs WHERE name='Bella'), '2025-06-10 09:30:00', 45, 'Beachside Ave', 'accepted'),
   ((SELECT id FROM Dogs WHERE name='largedog'), '2025-06-10 10:00:00', 60, 'Central Park', 'open'),
   ((SELECT id FROM Dogs WHERE name='mediumdog'), '2025-06-11 07:30:00', 20, 'Maple Street', 'open'),
-  ((SELECT id FROM Dogs WHERE name='smalldog'), '2025-06-11 09:00:00', 40, 'Oak Avenue', 'open');
+  ((SELECT id FROM Dogs WHERE name='smalldog'), '2025-06-12 09:00:00', 40, 'Oak Avenue', 'open');
