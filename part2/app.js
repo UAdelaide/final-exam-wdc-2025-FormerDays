7b1f56719878b5fe07e163fc349a9b4c6a5bde5b
FormerDays <tangpuran@outlook.com>
2025-06-20 23:07:05 +0000

    [automatic commit] 2025-06-20 23:07:04 UTC

--- part2/app.js
+++ part2/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-const session = require('express-session'); // 13:
+const session = require('express-session'); // 13: Session management
 require('dotenv').config();
 
 const app = express();
