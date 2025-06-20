e08d38dc38c186438de8e5088529d2eb2a49bc9b
FormerDays <tangpuran@outlook.com>
2025-06-20 16:03:47 +0000

    [automatic commit] 2025-06-20 16:03:46 UTC

--- part1/app.js
+++ part1/app.js
@@ -43,7 +43,8 @@ async function startServer() {
     app.get('/api/dogs', async (req, res) => {
         try {
             const [rows] = await connection.query('
-                SELECT d.name AS dog_name, d.size, u.username AS  FROM Dogs
+                SELECT d.name AS dog_name, d.size, u.username AS owner_username
+                FROM Dogs d
                 ');
             res.json(rows);
         } catch (err) {
