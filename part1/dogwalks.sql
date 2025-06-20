ccff4ebd31159947a77975083b19f48ae9809063
FormerDays <tangpuran@outlook.com>
2025-06-20 16:25:30 +0000

    [automatic commit] 2025-06-20 16:25:28 UTC

--- part1/dogwalks.sql
+++ part1/dogwalks.sql
@@ -70,7 +70,7 @@ VALUES
   ('mediumdog', 'medium', (SELECT user_id FROM Users WHERE username='carol123')),
   ('smalldog', 'small', (SELECT user_id FROM Users WHERE username='alice123'));
 
-INSERT INTO WalkRequests (dog_id, datetime, duration, location, status)
+INSERT INTO WalkRequests (dog_id, requested_time, duration_minutes, location, status)
 VALUES
   ((SELECT dog_id FROM Dogs WHERE name='Max'), '2025-06-10 08:00:00', 30, 'Parklands', 'open'),
   ((SELECT dog_id FROM Dogs WHERE name='Bella'), '2025-06-10 09:30:00', 45, 'Beachside Ave', 'accepted'),
