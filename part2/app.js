700578a1d9f14daf343f5e445bf0d573ba54745c
FormerDays <tangpuran@outlook.com>
2025-06-20 21:12:43 +0000

    [automatic commit] 2025-06-20 21:12:42 UTC

--- part2/app.js
+++ part2/app.js
@@ -9,6 +9,9 @@ const app = express();
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '/public')));
 
+// Session configuration
+app.use(session({
+
 // Routes
 const walkRoutes = require('./routes/walkRoutes');
 const userRoutes = require('./routes/userRoutes');
